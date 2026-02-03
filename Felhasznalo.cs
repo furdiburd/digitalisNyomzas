bfbf0311b5e00ae7bf8f66ce9df55d05539f9418
YOLOOOO <lets@go>
2026-02-03 20:35:41 +0100

    Felhasznalo.cs

--- Felhasznalo.cs
+++ Felhasznalo.cs
@@ -13,17 +13,28 @@ namespace digitalisNyomzas
 		private string azonosito;
 		private string szerepkor;
 
-		public Felhasznalo(string adattagok, string nev, string azonosito, string szerepkor)
-		{
-			this.adattagok = adattagok;
-			this.nev = nev;
-			this.azonosito = azonosito;
-			this.szerepkor = szerepkor;
-		}
-
 		public string Adattagok { get => adattagok; set => adattagok = value; }
 		public string Nev { get => nev; set => nev = value; }
 		public string Azonosito { get => azonosito; set => azonosito = value; }
 		public string Szerepkor { get => szerepkor; set => szerepkor = value; }
 	}
+
+    public class Felhasznalo
+    {
+        public string Nev { get; set; }
+        public string Azonosito { get; set; }
+        public string Szerepkor { get; set; }
+
+        public Felhasznalo(string nev, string azonosito, string szerepkor)
+        {
+            Nev = nev;
+            Azonosito = azonosito;
+            Szerepkor = szerepkor;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Azonosito}] {Nev} - {Szerepkor}";
+        }
+    }
 }
