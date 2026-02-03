dbec98bf53d4bd2e3f19ffe7f517b0a656a6708e
YOLOOOO <lets@go>
2026-02-03 21:12:40 +0100

    Ügy állapot

--- UgyAllapota.cs
+++ UgyAllapota.cs
@@ -6,8 +6,33 @@ using System.Threading.Tasks;
 
 namespace digitalisNyomzas
 {
-	internal class UgyAllapota
-	{
-		private List<string> cases;
-	}
+    public class UgyAllapota
+    { // nem a legszebb de jó lesz ez így
+        public string AktualisStatusz { get; private set; } 
+
+        public UgyAllapota(string kezdoStatusz = "nyitott")
+        {
+            AktualisStatusz = kezdoStatusz;
+        }
+
+        public void AllapotValtas(string ujStatusz)
+        {
+            // ellenőrzés hogy csak "nyitott / folyamatban / lezárt" státuszokra lehessen váltani
+            string[] validStatuszok = { "nyitott", "folyamatban", "lezárt" };
+
+            if (Array.Exists(validStatuszok, s => s == ujStatusz.ToLower()))
+            {
+                AktualisStatusz = ujStatusz.ToLower();
+            }
+            else
+            {
+                Console.WriteLine("Hiba: Érvénytelen státusz!");
+            }
+        }
+
+        public override string ToString()
+        {
+            return AktualisStatusz;
+        }
+    }
 }
